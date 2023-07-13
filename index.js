76c7169ee72e692d51c3215ad973ea483e245c99
elmaqwerty14 <elmakiu333@gmail.com>
2023-07-14 03:12:59 +0700

    create file connection.js

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
-
+const pool = require('./express/connection.js');
 const app = express()
 const port = 3000
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-app.get ('/', function (req, res) {
-   res.send('Hello World!')
+app.get('/', (req, res) => {
+  // query database
+  pool.query('SELECT * FROM tabelpertama', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.send(results.rows);
+  });
 })
+
+// app.get ('/', function (req, res) {
+//    res.send('Hello World!')
+// })
 
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`)
