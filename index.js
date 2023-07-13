462408f7a78177b132dc5efeb354a3cfd6ea1cfd
elmaqwerty14 <elmakiu333@gmail.com>
2023-07-13 11:51:28 +0700

    edit file index.js

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require('express');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+var ap = function (req, res) {
+   res.send('Hello World!')
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
