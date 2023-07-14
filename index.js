6a8600a3ad44275c4063db366407cf3fb4918546
elmaqwerty14 <elmakiu333@gmail.com>
2023-07-14 14:52:34 +0700

    update diles in folder express

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
-const express = require('express');
-const pool = require('./express/connection.js');
-const app = express()
-const port = 3000
+// const express = require('express');
+// const pool = require('./connection.js');
+// const app = express()
+// const port = 3000
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
+//   // query database
+//   pool.query('SELECT * FROM tabelpertama', (error, results) => {
+//     if (error) {
+//       throw error;
+//     }
+//     res.send(results.rows);
+//   });
+// })
+
+// // app.get ('/', function (req, res) {
+// //    res.send('Hello World!')
+// // })
+
+// // app.listen(port, () => {
+// //   console.log(`Example app listening on port ${port}`)
+// // })
+
+// app.listen(port, function()  {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+const express = require("express");
+
+const pool = require("./connection.js");
+
+const app = express();
+const port = 3000;
+//  console.log(pool);
+app.get("/", (req, res) => {
   // query database
-  pool.query('SELECT * FROM tabelpertama', (error, results) => {
+  pool.query("SELECT * FROM tabelpertama", (error, results) => {
     if (error) {
       throw error;
     }
     res.send(results.rows);
   });
-})
+});
 
-// app.get ('/', function (req, res) {
-//    res.send('Hello World!')
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
-
-app.listen(port, function()  {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}`);
+});
