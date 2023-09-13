9dbca47bba82a11f17ad6fc7b0f8fffc375ea868
elmaqwerty14 <elmakiu333@gmail.com>
2023-09-13 17:00:22 +0700

    create route register

--- toko/search/user/user.service.js
+++ toko/search/user/user.service.js
@@ -75,4 +75,43 @@ router.put('/editUser/:id', (req, res) => {
     });
 });
 
+router.post('/registerUser', (req, res) => {
+  const { nama_pembeli, token_user } = req.body;
+
+  // Periksa apakah nama_pembeli atau token_user sudah terdaftar sebelumnya
+  const checkQuery = {
+    text: 'SELECT * FROM user WHERE nama_pembeli = $1 OR token_user = $2',
+    values: [nama_pembeli, token_user],
+  };
+
+  client.query(checkQuery)
+    .then(result => {
+      if (result.rows.length > 0) {
+        // Jika nama_pembeli atau token_user sudah terdaftar, kirim respons error
+        res.status(400).json({ error: 'Nama pengguna atau token sudah terdaftar' });
+      } else {
+        // Jika tidak ada nama_pembeli atau token_user yang sama, lakukan penyisipan
+        const insertQuery = {
+          text: 'INSERT INTO user (nama_pembeli, token_user) VALUES ($1, $2)',
+          values: [nama_pembeli, token_user],
+        };
+
+        client.query(insertQuery)
+          .then(() => {
+            console.log('Pengguna berhasil terdaftar');
+            res.status(201).json({ message: 'Pengguna berhasil terdaftar' });
+          })
+          .catch(err => {
+            console.error('Kesalahan saat mendaftarkan pengguna:', err);
+            res.status(500).json({ error: 'Kesalahan saat mendaftarkan pengguna' });
+          });
+      }
+    })
+    .catch(err => {
+      console.error('Kesalahan saat memeriksa pengguna yang sudah terdaftar:', err);
+      res.status(500).json({ error: 'Kesalahan saat memeriksa pengguna yang sudah terdaftar' });
+    });
+});
+
+
 module.exports = router;
