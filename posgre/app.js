6be6d5a09400eba93ba0d8b3bea693ea39703fb3
elmaqwerty14 <elmakiu333@gmail.com>
2023-09-20 23:15:06 +0700

    edit

--- posgre/app.js
+++
@@ -1,8 +0,0 @@
-// ./app.js
-import express from 'express'
-import mountRoutes from './routes.js'
- 
-const app = express()
-mountRoutes(app)
- 
-// ... more express setup stuff can follow
--- posgre/db/index.js
+++
@@ -1,5 +0,0 @@
-import { Pool } from 'pg'
- 
-const pool = new Pool()
- 
-export const query = (text, params) => pool.query(text, params);
